6d7c3434d856ac41ba8e70ee164815eea2604f06
Maxence Delattre <maxence.delattre@woven-planet.global>
2025-03-03 09:31:01 +0900

    Free increment

--- src/main/java/wbyt/qtest2/App.java
+++ src/main/java/wbyt/qtest2/App.java
@@ -1,10 +1,38 @@
 package wbyt.qtest2;
 
+import static java.lang.System.currentTimeMillis;
+
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+
+	private static final Logger logger = Logger.getLogger("Default");
+
+	public static void log(String s, Object... args) {
+		logger.info(String.format(s, args));
+	}
+
+	public static void main(String[] args) {
+		long start;
+		long end;
+
+		start = currentTimeMillis();
+		freeIncrement();
+		end = currentTimeMillis();
+		log("freeIncrement: %d", end - start);
+		
+
+	}
+
+	public static void freeIncrement() {
+		// Example function logic
+		long ctr = 0;
+		for (long i = 0; i < 500000000; i++) {
+			ctr++;
+		}
+		assert (ctr == 500000000);
+	}
 }
